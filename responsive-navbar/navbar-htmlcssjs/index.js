b349dfc1351e2d351d99bc432630f389b9f88c2e
itstahirrafique <hello.tahirrafique@gmail.com>
2025-04-13 19:46:34 +0500

    navbar added

---
+++ responsive-navbar/navbar-htmlcssjs/index.js
@@ -0,0 +1,11 @@
+console.log('hello Tahir!')
+
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none'
+}
